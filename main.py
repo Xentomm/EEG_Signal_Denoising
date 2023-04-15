92e8efae19e56a358e0464db0bf654c337a5bc92
Xentomm <erimuslukasz@gmail.com>
2023-04-15 11:47:24 +0200

    Imports

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-# init
+import numpy as np
+import matplotlib.pyplot as plt
+import pyedflib
+import sklearn.linear_model as slm
+from sklearn import metrics
+from statsmodels.tsa.ar_model import AutoReg
+import scipy
+import scipy.signal as sig
+from sklearn.decomposition import FastICA, PCA
+import pywt
+
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
